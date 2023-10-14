9e5468bc5d873aa588d9aa569457447d1aa098c4
Darshan <karkeradarshan98@gmail.com>
2023-10-14 10:14:12 -0500

    Report Page updated with buttons

--- src/components/ReportPage/ReportPage.js
+++ src/components/ReportPage/ReportPage.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "@mdi/font/css/materialdesignicons.css";
 
 const ReportPage = () => {
-    return <div className="report-page"></div>;
+    const handleShareClick = () => {
+        // Open Outlook app to share the report
+        window.location.href = "ms-outlook:?subject=Share%20Report";
+    };
+
+    const handleDownloadClick = () => {
+        // download the report generated from backend
+    };
+
+    const handleFeedbackClick = () => {
+        window.location.href = "/feedback";
+    };
+
+    return (
+        <div className="report-page">
+            <h1>Reports</h1>
+            <div className="buttons">
+                <button onClick={handleShareClick}>
+                    <i className="mdi mdi-share-variant"></i>
+                    Share
+                </button>
+                <button onClick={handleDownloadClick}>
+                    <i className="mdi mdi-download"></i>
+                    Download
+                </button>
+                <button onClick={handleFeedbackClick}>
+                    <i className="mdi mdi-feedback"></i>
+                    Feedback
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ReportPage;
