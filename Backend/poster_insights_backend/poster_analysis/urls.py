e4ed08698d342c0957f02b7ffe5437ca9a937bad
Darshan <karkeradarshan98@gmail.com>
2023-10-29 13:44:16 -0500

    backend color analysis functionality changes

--- Backend/poster_insights_backend/poster_analysis/urls.py
+++ Backend/poster_insights_backend/poster_analysis/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from .views import upload_poster, start_analysis, get_report
+from .views import generate_pdf_report, upload_poster, get_report
 
 urlpatterns = [
     path('upload-poster/', upload_poster, name='upload_poster'),
-    path('start-analysis/', start_analysis, name='start_analysis'),
     path('get-report/', get_report, name='get_report'),
+    path('generate_pdf_report/', generate_pdf_report, name='generate_pdf_report'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Backend/poster_insights_backend/poster_analysis/views.py
+++ Backend/poster_insights_backend/poster_analysis/views.py
@@ -6,9 +6,13 @@ from PIL import Image
 import pytesseract
 from io import BytesIO
 from transformers import pipeline
-import cv2  
+import cv2
 import numpy as np
 from django.conf import settings
+import re
+import requests
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
@@ -19,100 +23,116 @@ image_quality = {}
 
 @csrf_exempt
 def upload_poster(request):
-    global extracted_text  
-    global image_quality 
+    global extracted_text, image_quality
     if request.method == 'POST':
-        # Get the uploaded image file.
         image_file = request.FILES['image']
-
-        # Create a temporary file to save the uploaded image.
         temp_image_path = os.path.join(settings.MEDIA_ROOT, 'temp_image.jpg')
 
         with open(temp_image_path, 'wb') as temp_image:
             for chunk in image_file.chunks():
                 temp_image.write(chunk)
 
-        # Open the temporary image file and extract text
         image = Image.open(temp_image_path)
         extracted_text = pytesseract.image_to_string(image)
-
-        # Calculate image quality metrics using OpenCV
         image_data = cv2.imread(temp_image_path)
         image_height, image_width, _ = image_data.shape
-
-        # Calculate image sharpness (variance of Laplacian)
         gray = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
         laplacian = cv2.Laplacian(gray, cv2.CV_64F)
         sharpness = laplacian.var()
-
-        # Calculate image contrast
         contrast = cv2.convertScaleAbs(laplacian).mean()
-
-        # Update image quality metrics
-        image_quality = {
-            "Resolution": f"{image_width}x{image_height}",
-            "Sharpness": sharpness,
-            "Contrast": contrast
-        }
-
-        # Summarize the extracted text
+        image_quality = {"Resolution": f"{image_width}x{image_height}", "Sharpness": sharpness, "Contrast": contrast}
         global summarized_text
-        summarized_text = summarizer(extracted_text, max_length=30, min_length=10, do_sample=False)
-
+        summarized_text = summarizer(extracted_text, max_length=35, min_length=10, do_sample=False)
         return JsonResponse({'message': 'Poster uploaded successfully.'})
-
     return HttpResponse('Method not allowed.')
 
-@csrf_exempt
-def start_analysis(request):
-    global extracted_text
-    global summarized_text
-    global image_quality
+def extract_colors(image):
+    image_array = np.array(image)
 
-    return JsonResponse({'message': 'Analysis started'})
+    # Calculate the unique colors in the image.
+    unique_colors, counts = np.unique(image_array.reshape(-1, image_array.shape[2]), axis=0, return_counts=True)
+
+    # Convert the unique colors to RGB hex codes and calculate percentages.
+    total_pixels = np.sum(counts)
+    color_data = {}
+
+    for color, count in zip(unique_colors, counts):
+        hex_color = '#{0:02X}{1:02X}{2:02X}'.format(color[0], color[1], color[2])
+        percentage = (count / total_pixels) * 100
+        color_data[hex_color] = percentage
+    sorted_colors = sorted(color_data.items(), key=lambda x: x[1], reverse=True)
+   # Extract the top 10 major colors
+    top_10_colors = sorted_colors[:10]
+    return top_10_colors
+
+
 
 @csrf_exempt
 def get_report(request):
-    global extracted_text
-    global summarized_text
-    global image_quality
+  global extracted_text, summarized_text, image_quality
 
-    report = {
-        "report": [
-            {
-                "sectionName": "Content Summary",
-                "data": [summarized_text[0]['summary_text']]
-            },
-            {
-                "sectionName": "Extracted Text",
-                "data": [extracted_text]
-            },
-            {
-                "sectionName": "Link Validation",
-                "data": {
-                    "link1": True,  
-                    "link2": False  
-                }
-            },
-            {
-                "sectionName": "Image Quality",
-                "data": image_quality  
-            },
-            {
-                "sectionName": "Text Quality",
-                "data": {
-                    "Font Used": "Arial",  
-                    "Text Size": "Small",  
-                    "Recommendation": "Use a bigger text size."  
-                }
-            },
-            {
-                "sectionName": "Color Analysis",
-                "data": {
-                    "Color Relevance Measure": "High",  
-                    "Recommendations": "Text and background colors are too similar. Consider using a higher contrast background color (e.g., black on white or vice versa)."  # Replace with actual recommendations
-                }
-            }
-        ]
-    }
-    return JsonResponse(report)
+  def validate_links(text):
+    links = re.findall(r'http[s]?://\S+', text)
+    results = {}
+    for i, link in enumerate(links):
+      try:
+        response = requests.head(link)
+        results[f"link{i + 1}"] = response.status_code == 200
+      except Exception as e:
+        results[f"link{i + 1}"] = False
+    return results
+
+  link_validation_results = validate_links(extracted_text)
+
+  # Extract the colors in the image.
+  image = Image.open(os.path.join(settings.MEDIA_ROOT, 'temp_image.jpg'))
+  color_data = extract_colors(image)
+
+  report = {
+    "report": [
+      {"sectionName": "Content Summary", "data": [summarized_text[0]['summary_text']]},
+      {"sectionName": "Extracted Text", "data": [extracted_text]},
+      {"sectionName": "Link Validation", "data": link_validation_results},
+      {"sectionName": "Image Quality", "data": image_quality},
+      {"sectionName": "Text Quality", "data": {"Font Used": "Arial", "Text Size": "Small", "Recommendation": "Use a bigger text size."}},
+      {"sectionName": "Color Analysis", "data": color_data},
+    ]
+  }
+
+  return JsonResponse(report)
+
+
+@csrf_exempt
+def generate_pdf_report(request):
+    global extracted_text, summarized_text, image_quality
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    c = canvas.Canvas(response, pagesize=letter)
+
+    content = [
+        "Content Summary:",
+        summarized_text[0]['summary_text'],
+        "\nExtracted Text:",
+        extracted_text,
+        "\nImage Quality:",
+        f"Resolution: {image_quality['Resolution']}",
+        f"Sharpness: {image_quality['Sharpness']}",
+        f"Contrast: {image_quality['Contrast']}",
+        "\nText Quality:",
+        "Font Used: Arial",
+        "Text Size: Small",
+        "Recommendation: Use a bigger text size.",
+        "\nColor Analysis:",
+        "Color Relevance Measure: High",
+        "Recommendations: Use higher contrast background colors."
+    ]
+
+    y = 750
+    for line in content:
+        c.drawString(100, y, line)
+        y -= 15
+
+    c.showPage()
+    c.save()
+
+    return response
